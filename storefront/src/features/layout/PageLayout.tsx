994d94dac72258dfaf7b0ee1a20cdfdbdb1c065b
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2023-05-10 21:00:09 +0200

    Finished product page layout

--- storefront/src/features/layout/PageLayout.tsx
+++ storefront/src/features/layout/PageLayout.tsx
@@ -25,7 +25,7 @@ const PageLayout = ({ children, title }: PageLayoutProps) => {
         alignItems="center"
         overflow="auto"
       >
-        <Flex maxWidth="1536px" w="100%" direction="column">
+        <Flex maxWidth="1536px" w="100%" direction="column" gap={10}>
           {children}
         </Flex>
       </Flex>
