976f7482433fb9f2796eee173297690ee2918d4b
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2023-05-16 20:52:49 +0200

    Started working on checkout page

--- storefront/src/pages/checkout/index.tsx
+++ storefront/src/pages/checkout/index.tsx
@@ -1,7 +1,46 @@
 import { PageLayout } from "@/features/layout";
+import { Grid, GridItem, Flex } from "@chakra-ui/react";
 
 const CheckoutPage = () => {
-  return <PageLayout title="Kasa"></PageLayout>;
+  return (
+    <PageLayout title="Kasa">
+      <Grid
+        templateColumns={[
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(2, 1fr)",
+          "repeat(2, 1fr)",
+          "repeat(3, 1fr)",
+        ]}
+        gap={6}
+      >
+        <GridItem colSpan={[1, 1, 1, 1, 2]}>
+          <Grid
+            templateColumns={[
+              "repeat(1, 1fr)",
+              "repeat(1, 1fr)",
+              "repeat(1, 1fr)",
+              "repeat(1, 1fr)",
+              "repeat(2, 1fr)",
+            ]}
+            gap={6}
+          >
+            <GridItem bgColor="red.200">
+              <Flex>Shipping</Flex>
+            </GridItem>
+
+            <GridItem bgColor="green.200">
+              <Flex>Payment</Flex>
+            </GridItem>
+          </Grid>
+        </GridItem>
+
+        <GridItem bgColor="blue.200">
+          <Flex>Summary</Flex>
+        </GridItem>
+      </Grid>
+    </PageLayout>
+  );
 };
 
 export default CheckoutPage;
