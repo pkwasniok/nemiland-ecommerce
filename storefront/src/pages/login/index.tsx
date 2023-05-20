28bee50d2338b3673f0a0e2a0586781a721de2d0
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2023-05-20 17:10:53 +0200

    Redesigned register page

--- storefront/src/pages/login/index.tsx
+++ storefront/src/pages/login/index.tsx
@@ -87,41 +87,6 @@ const LoginPage = () => {
       </Flex>
     </PageLayout>
   );
-
-  // return (
-  //   <PageLayout title="Logowanie" showTitle>
-  //     <LoginWidget
-  //       onSuccess={() => router.push("/account")}
-  //       onError={(e) => (e == "NotVerifiedError" ? router.push("/verify") : {})}
-  //     />
-
-  //     <Flex alignItems="center" gap={4}>
-  //       <Divider />
-  //       <Text fontSize="sm" textColor="gray.400">
-  //         Lub
-  //       </Text>
-  //       <Divider />
-  //     </Flex>
-
-  //     <Button
-  //       variant="ghost"
-  //       justifyContent="start"
-  //       as={NextLink}
-  //       href="/register"
-  //     >
-  //       Nie masz konta? Zarejestruj się
-  //     </Button>
-
-  //     <Button
-  //       variant="ghost"
-  //       justifyContent="start"
-  //       as={NextLink}
-  //       href="/password-reset"
-  //     >
-  //       Nie pamiętasz hasła? Zresetuj hasło
-  //     </Button>
-  //   </PageLayout>
-  // );
 };
 
 export default LoginPage;
--- storefront/src/pages/register/index.tsx
+++ storefront/src/pages/register/index.tsx
@@ -1,14 +1,62 @@
 import { useRouter } from "next/router";
+import NextLink from "next/link";
 
 import { PageLayout } from "@/features/layout";
 import { RegisterWidget } from "@/features/account";
+import { Flex, Heading, Button } from "@chakra-ui/react";
 
 const RegisterPage = () => {
   const router = useRouter();
 
   return (
     <PageLayout title="Rejestracja" backlinkHref="/login" showTitle>
-      <RegisterWidget onSuccess={() => router.push("/login")} />
+      <Flex
+        minH="92vh"
+        direction={["column", "column", "column", "row"]}
+        gap={[6, 6, 6, 0]}
+      >
+        <Flex
+          flex={1}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex
+            maxW={["", "", "", "450px"]}
+            w="100%"
+            p={6}
+            bgColor="white"
+            direction="column"
+            gap={6}
+          >
+            <Heading size="md">Rejestracja</Heading>
+
+            <RegisterWidget onSuccess={() => router.push("/login")} />
+          </Flex>
+        </Flex>
+
+        <Flex
+          flex={1}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex
+            maxW={["", "", "", "450px"]}
+            w="100%"
+            p={6}
+            bgColor="white"
+            direction="column"
+            gap={6}
+          >
+            <Heading size="md">Masz już konto?</Heading>
+
+            <Button as={NextLink} href="/login">
+              Zaloguj się
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
     </PageLayout>
   );
 };
