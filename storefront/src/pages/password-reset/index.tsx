ba9de44fb882b92c53118272c3e08e4677b38403
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2023-05-20 17:21:49 +0200

    Redesigned password reset and verification page

--- storefront/src/pages/password-reset/index.tsx
+++ storefront/src/pages/password-reset/index.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import NextLink from "next/link";
 
 import { PageLayout } from "@/features/layout";
 import {
   PasswordResetWidget,
   RequestPasswordResetWidget,
 } from "@/features/account";
+import { Flex, Heading, Button } from "@chakra-ui/react";
 
 const PasswordResetPage = () => {
   const [view, setView] = useState<"RequestPasswordReset" | "PasswordReset">(
@@ -22,21 +24,48 @@ const PasswordResetPage = () => {
 
   return (
     <PageLayout title="Resetowanie hasła" backlinkHref="/login" showTitle>
-      {view == "RequestPasswordReset" && (
-        <RequestPasswordResetWidget onSuccess={() => router.push("/login")} />
-      )}
+      <Flex
+        minH="92vh"
+        direction="column"
+        alignItems="center"
+        justifyContent={["", "", "", "center"]}
+      >
+        <Flex
+          maxW={["", "", "", "450px"]}
+          w="100%"
+          p={6}
+          direction="column"
+          gap={6}
+          borderRadius={6}
+          bgColor="white"
+        >
+          <Heading size="md">Resetowanie hasła</Heading>
 
-      {view == "PasswordReset" && (
-        <PasswordResetWidget
-          token={router.query.token as string}
-          onSuccess={() => router.push("/login")}
-          onError={(e) =>
-            e == "InvalidTokenError"
-              ? setView("RequestPasswordReset")
-              : router.push("/verify")
-          }
-        />
-      )}
+          {view == "RequestPasswordReset" && (
+            <>
+              <RequestPasswordResetWidget
+                onSuccess={() => router.push("/login")}
+              />
+
+              <Button as={NextLink} href="/login">
+                Zaloguj się
+              </Button>
+            </>
+          )}
+
+          {view == "PasswordReset" && (
+            <PasswordResetWidget
+              token={router.query.token as string}
+              onSuccess={() => router.push("/login")}
+              onError={(e) =>
+                e == "InvalidTokenError"
+                  ? setView("RequestPasswordReset")
+                  : router.push("/verify")
+              }
+            />
+          )}
+        </Flex>
+      </Flex>
     </PageLayout>
   );
 };
--- storefront/src/pages/verify/index.tsx
+++ storefront/src/pages/verify/index.tsx
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useMutation } from "@apollo/client";
 import { GQL_MUTATION_VERIFY } from "@/lib/vendure";
+import NextLink from "next/link";
 
 import { PageLayout } from "@/features/layout";
 import { RequestVerificationWidget } from "@/features/account";
-import { useToast } from "@chakra-ui/react";
+import { useToast, Flex, Heading, Button } from "@chakra-ui/react";
 
 const VerificationPage = () => {
   const router = useRouter();
@@ -50,7 +51,30 @@ const VerificationPage = () => {
 
   return (
     <PageLayout title="Weryfikacja konta" backlinkHref="/login" showTitle>
-      <RequestVerificationWidget onSuccess={() => router.push("/login")} />
+      <Flex
+        minH="92vh"
+        direction="column"
+        alignItems="center"
+        justifyContent={["", "", "", "center"]}
+      >
+        <Flex
+          maxW={["", "", "", "450px"]}
+          w="100%"
+          p={6}
+          direction="column"
+          gap={6}
+          borderRadius={6}
+          bgColor="white"
+        >
+          <Heading size="md">Weryfikacja konta</Heading>
+
+          <RequestVerificationWidget onSuccess={() => router.push("/login")} />
+
+          <Button as={NextLink} href="/login">
+            Zaloguj się
+          </Button>
+        </Flex>
+      </Flex>
     </PageLayout>
   );
 };
