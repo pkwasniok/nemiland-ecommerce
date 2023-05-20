77b576fd16d3293679a6ac537c21a2cca76a5dec
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2023-05-20 14:58:18 +0200

    Redesigned login page

--- storefront/src/pages/login/index.tsx
+++ storefront/src/pages/login/index.tsx
@@ -3,45 +3,125 @@ import NextLink from "next/link";
 
 import { PageLayout } from "@/features/layout";
 import { LoginWidget } from "@/features/account";
-import { Flex, Text, Divider, Button } from "@chakra-ui/react";
+import { Flex, Button, Heading } from "@chakra-ui/react";
+
+import { FiMail, FiSearch, FiList, FiEdit3 } from "react-icons/fi";
 
 const LoginPage = () => {
   const router = useRouter();
 
   return (
-    <PageLayout title="Logowanie" showTitle>
-      <LoginWidget
-        onSuccess={() => router.push("/account")}
-        onError={(e) => (e == "NotVerifiedError" ? router.push("/verify") : {})}
-      />
+    <PageLayout title="Logowanie">
+      <Flex
+        minH="92vh"
+        direction={["column", "column", "column", "row"]}
+        gap={[6, 6, 6, 0]}
+      >
+        <Flex
+          flex={1}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex
+            maxW={["", "", "", "450px"]}
+            w="100%"
+            p={6}
+            bgColor="white"
+            direction="column"
+            gap={6}
+          >
+            <Heading size="md">Logowanie</Heading>
 
-      <Flex alignItems="center" gap={4}>
-        <Divider />
-        <Text fontSize="sm" textColor="gray.400">
-          Lub
-        </Text>
-        <Divider />
+            <LoginWidget onSuccess={() => router.push("/account")} />
+
+            <Button as={NextLink} href="/password-reset">
+              Zresetuj hasło
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex
+          flex={1}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex
+            maxW={["", "", "", "450px"]}
+            w="100%"
+            p={6}
+            bgColor="white"
+            direction="column"
+            gap={6}
+          >
+            <Heading size="md">Nie masz jeszcze konta?</Heading>
+
+            <Flex px={3} direction="column" gap={3} textColor="gray.600">
+              <Flex gap={3} alignItems="center" fontSize="sm">
+                <FiEdit3 size={20} />
+                Szybszy proces składania zamówień
+              </Flex>
+
+              <Flex gap={3} alignItems="center" fontSize="sm">
+                <FiMail size={20} />
+                Powiadomienia o nowościach i promocjach
+              </Flex>
+
+              <Flex gap={3} alignItems="center" fontSize="sm">
+                <FiSearch size={20} />
+                Śledzenie statusu zamówienia
+              </Flex>
+
+              <Flex gap={3} alignItems="center" fontSize="sm">
+                <FiList size={20} />
+                Historia zakupów
+              </Flex>
+            </Flex>
+
+            <Button as={NextLink} href="/register">
+              Utwórz konto
+            </Button>
+          </Flex>
+        </Flex>
       </Flex>
-
-      <Button
-        variant="ghost"
-        justifyContent="start"
-        as={NextLink}
-        href="/register"
-      >
-        Nie masz konta? Zarejestruj się
-      </Button>
-
-      <Button
-        variant="ghost"
-        justifyContent="start"
-        as={NextLink}
-        href="/password-reset"
-      >
-        Nie pamiętasz hasła? Zresetuj hasło
-      </Button>
     </PageLayout>
   );
+
+  // return (
+  //   <PageLayout title="Logowanie" showTitle>
+  //     <LoginWidget
+  //       onSuccess={() => router.push("/account")}
+  //       onError={(e) => (e == "NotVerifiedError" ? router.push("/verify") : {})}
+  //     />
+
+  //     <Flex alignItems="center" gap={4}>
+  //       <Divider />
+  //       <Text fontSize="sm" textColor="gray.400">
+  //         Lub
+  //       </Text>
+  //       <Divider />
+  //     </Flex>
+
+  //     <Button
+  //       variant="ghost"
+  //       justifyContent="start"
+  //       as={NextLink}
+  //       href="/register"
+  //     >
+  //       Nie masz konta? Zarejestruj się
+  //     </Button>
+
+  //     <Button
+  //       variant="ghost"
+  //       justifyContent="start"
+  //       as={NextLink}
+  //       href="/password-reset"
+  //     >
+  //       Nie pamiętasz hasła? Zresetuj hasło
+  //     </Button>
+  //   </PageLayout>
+  // );
 };
 
 export default LoginPage;
